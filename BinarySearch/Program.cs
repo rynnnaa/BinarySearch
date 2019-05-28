ed5d9535a69d4eb6bac03eb55e88d8919abb6aeb
Tulip Pink <pinktulips@hotmail.com>
2019-05-28 12:43:52 -0700

    argument

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -6,7 +6,16 @@ namespace BinarySearch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var myList = new List<int> { 1, 4, 5, 6, 9 };
+            Console.WriteLine(BinarySearch(myList, 3));
+            Console.WriteLine(BinarySearch(myList, -1));
+
         }
     }
+
+    int BinarySearch(int list[], int item, int len)
+    {
+
+    }
+
 }
